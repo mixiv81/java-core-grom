77c7431838d7484333b5e6229b367065975f1fd3
mixiv81 <zhluktenkoiv@gmail.com>
2018-03-25 18:37:42 +0300

    init

---
+++ src/lesson15/HW/APIandImplementation/GoogleAPI.java
@@ -0,0 +1,36 @@
+package lesson15.HW.APIandImplementation;
+
+public class GoogleAPI implements API {
+    Room[] rooms;
+
+    public GoogleAPI(Room[] rooms) {
+        this.rooms = rooms;
+    }
+
+    @Override
+    public Room[] findRooms(int price, int persons, String city, String hotel) {
+        Room room1 = new Room(price, persons, city, hotel);
+        int countedArrInd = 0;
+        for (Room room : rooms) {
+            if (room != null && room1.equals(room) && room1.hashCode() == room.hashCode()) {
+                    countedArrInd++;
+                }
+            }
+
+        Room[] roomsParam = new Room[countedArrInd];
+        int index = 0;
+        for (Room room : rooms) {
+            if (room != null && room1.equals(room) && room1.hashCode() == room.hashCode()) {
+                    roomsParam[index] = room;
+                    index++;
+                }
+            }
+        return roomsParam;
+    }
+
+
+    @Override
+    public Room[] getAll() {
+        return rooms;
+    }
+}
---
+++ src/lesson15/equals/User.java
@@ -0,0 +1,9 @@
+package lesson15.equals;
+
+public class User {
+    private long id;
+
+    public User(int id) {
+        this.id = id;
+    }
+}
