d4127b4c4153491f1f4d91dbe8180974c9fd3518
mixiv81 <37148638+mixiv81@users.noreply.github.com>
2018-03-26 13:54:21 +0300

    Update UserRepository.java

--- src/lesson15/HW/UserRepositiry/UserRepository.java
+++ src/lesson15/HW/UserRepositiry/UserRepository.java
@@ -14,28 +14,22 @@ public class UserRepository {
         return users;
     }
 
-    public int countArrInd() {
-        int count = 0;
-        if (users != null)
-            for (User user : users) {
-                if (user != null)
-                    count++;
-            }
-        return count;
-    }
-
-
     public User findById(long id) {
         if (users != null)
-            for (int i = 0; i < users.length; i++) {
-                if (users[i] != null && users[i].getId() == id)
-                    return users[i];
+            for (User user : users) {
+                if (user != null && user.getId() == id)
+                    return user;
             }
         return null;
     }
 
     public User save(User user) {
-        if (user != null && findById(user.getId()) == null && countArrInd() != 0) {
+        if (user != null && findById(user.getId()) == null && users != null) {
+            for (User user1 : users) {
+                if (user1 != null && user1.equals(user) && user1.hashCode() == user.hashCode()) {
+                    return null;
+                }
+            }
             for (int i = 0; i < users.length; i++) {
                 if (users[i] == null) {
                     users[i] = user;
@@ -49,7 +43,7 @@ public class UserRepository {
     public User update(User user) {
         if (user != null && findById(user.getId()) != null) {
             for (int i = 0; i < users.length; i++) {
-                if (users[i] != null && users[i].getId() == user.getId()) {
+                if (users[i] != null && users[i].getId() == user.getId() && !users[i].equals(user) && users[i].hashCode() != user.hashCode()) {
                     users[i] = user;
                     return users[i];
                 }
