705842746b5b69fdc14c2805b67c49a56b42c419
Ihor <37148638+mixiv81@users.noreply.github.com>
2018-03-28 11:54:56 +0300

    Add files via upload

--- src/lesson17/Demo.java
+++ src/lesson17/Demo.java
@@ -15,20 +15,20 @@ public class Demo {
         String test5 = null + " hfgh ";
 
 
-        System.out.println("------------ countWords--------------");
-        System.out.println(solution.countWords(test));
-        System.out.println(solution.countWords(test1));
-        System.out.println(solution.countWords(test2));
-        System.out.println(solution.countWords(test3));
-        System.out.println(solution.countWords(test4));
-
-        System.out.println("------------ checkNumbersAndSymbols--------------");
-        System.out.println(solution.checkNumbersAndSymbols(test));
-        System.out.println(solution.checkNumbersAndSymbols(test1));
-        System.out.println(solution.checkNumbersAndSymbols(test2));
-        System.out.println(solution.checkNumbersAndSymbols(test3));
-        System.out.println(solution.checkNumbersAndSymbols(test4));
-        System.out.println(test2.length());
+//        System.out.println("------------ countWords--------------");
+//        System.out.println(solution.countWords(test));
+//        System.out.println(solution.countWords(test1));
+//        System.out.println(solution.countWords(test2));
+//        System.out.println(solution.countWords(test3));
+//        System.out.println(solution.countWords(test4));
+//
+//        System.out.println("------------ checkNumbersAndSymbols--------------");
+//        System.out.println(solution.checkNumbersAndSymbols(test));
+//        System.out.println(solution.checkNumbersAndSymbols(test1));
+//        System.out.println(solution.checkNumbersAndSymbols(test2));
+//        System.out.println(solution.checkNumbersAndSymbols(test3));
+//        System.out.println(solution.checkNumbersAndSymbols(test4));
+//        System.out.println(test2.length());
 
         System.out.println("------------maxWord----------");
         System.out.println(solution.maxWord(test));
