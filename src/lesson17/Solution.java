3e51638801e489bec8bce9313439b1e1dd322d4e
Ihor <37148638+mixiv81@users.noreply.github.com>
2018-03-29 12:34:37 +0300

    Add files via upload

--- src/lesson17/Solution.java
+++ src/lesson17/Solution.java
@@ -16,9 +16,7 @@ public class Solution {
     }
 
     public String maxWord(String input) {
-        if (input == null)
-            return null;
-        if (input.isEmpty())
+        if (!valid(input))
             return null;
         String[] strings = input.split(" ");
         String findWord = "";
@@ -34,9 +32,7 @@ public class Solution {
     }
 
     public String minWord(String input) {
-        if (input == null)
-            return null;
-        if (input.isEmpty())
+        if (!valid(input))
             return null;
         String[] strings = input.split(" ");
         String findWord = "";
@@ -55,10 +51,37 @@ public class Solution {
         return findWord;
     }
 
+    public String mostCountedWord(String input) {
+        if (!valid(input))
+            return null;
+        String[] strings = input.split(" ");
+        String findWord = "";
+        int max = 0;
+        for (String string : strings) {
+            if (checkNumbersAndSymbols(string) && countDuplicates(string, strings) > max) {
+                findWord = string;
+                max = countDuplicates(string, strings);
+            }
+        }
+        if (valid(findWord))
+            return findWord;
+        else
+            return null;
+    }
+
+    public int countDuplicates(String string, String[] strings) {
+        if (!valid(string))
+            return 0;
+        int count = -1;
+        for (String stringElem : strings) {
+            if (stringElem.equals(string))
+                count++;
+        }
+        return count;
+    }
+
     private boolean checkNumbersAndSymbols(String input) {
-        if (input == null)
-            return false;
-        if (input.isEmpty())
+        if (!valid(input))
             return false;
         for (int i = 0; i < input.length(); i++) {
             if (!Character.isLetter(input.charAt(i)))
@@ -66,4 +89,8 @@ public class Solution {
         }
         return true;
     }
+    private boolean valid(String input) {
+        return input != null && !input.isEmpty();
+    }
+
 }
