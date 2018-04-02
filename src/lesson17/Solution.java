c7c97d0ab8298c6296ce49806964cbbc0fa8f0dc
Ihor <37148638+mixiv81@users.noreply.github.com>
2018-04-02 11:31:46 +0300

    Add files via upload

--- src/lesson17/Solution.java
+++ src/lesson17/Solution.java
@@ -1,16 +1,112 @@
 package lesson17;
 
 public class Solution {
-    public static void main(String[] args) {
-        String test = "https://www.gromcod77e.com";
-        System.out.println(validate(test));
+
+    public int countWords(String input) {
+        if (input == null)
+            return 0;
+        String[] strings = input.split(" ");
+        int count = 0;
+        for (String string : strings) {
+            if (checkNumbersAndSymbols(string))
+                count++;
+        }
+        return count;
     }
 
-    public static boolean validate(String address) {
-        return address != null && !address.isEmpty() && checkHttp(address) && checkDomen(address);
+    public String maxWord(String input) {
+        if (input == null)
+            return null;
+        if (input.isEmpty())
+            return null;
+        String[] strings = input.split(" ");
+        String findWord = "";
+        for (String string : strings) {
+            if (checkNumbersAndSymbols(string) && string.length() >= findWord.length()) {
+                findWord = string;
+            }
+        }
+        if (findWord.equals(""))
+            return null;
+        else
+            return findWord;
     }
 
-    private static boolean checkAddress(String input) {
+    public String minWord(String input) {
+        if (input == null)
+            return null;
+        if (input.isEmpty())
+            return null;
+        String[] strings = input.split(" ");
+        String findWord = "";
+        for (String string : strings) {
+            if (checkNumbersAndSymbols(string)) {
+                findWord = string;
+            }
+        }
+        if (findWord.equals(""))
+            return null;
+        for (String string : strings) {
+            if (checkNumbersAndSymbols(string) && findWord.length() > string.length()) {
+                findWord = string;
+            }
+        }
+        return findWord;
+    }
+
+    public String mostCountedWord(String input) {
+        if (input == null)
+            return null;
+        if (input.isEmpty())
+            return null;
+        String[] strings = input.toLowerCase().split(" ");
+        if (strings.length < 2)
+            return null;
+        String findWord = "";
+        int max = 1;
+        for (String string : strings) {
+            if (checkNumbersAndSymbols(string) && countDuplicates(string, strings) > max) {
+                findWord = string;
+                max = countDuplicates(string, strings);
+            }
+        }
+        if (!findWord.equals(""))
+            return findWord;
+        else
+            return null;
+    }
+
+    public boolean validate(String address) {
+        return address != null && !address.isEmpty() && checkHttp(address);
+    }
+
+
+    private int countDuplicates(String string, String[] strings) {
+        if (string == null)
+            return 0;
+        if (string.isEmpty())
+            return 0;
+        int count = 0;
+        for (String stringElem : strings) {
+            if (checkNumbersAndSymbols(string) && stringElem.equals(string))
+                count++;
+        }
+        return count;
+    }
+
+    private boolean checkNumbersAndSymbols(String input) {
+        if (input == null)
+            return false;
+        if (input.isEmpty())
+            return false;
+        for (int i = 0; i < input.length(); i++) {
+            if (!Character.isLetter(input.charAt(i)))
+                return false;
+        }
+        return true;
+    }
+
+    private boolean checkAddress(String input) {
         if (input == null)
             return false;
         if (input.isEmpty())
@@ -22,11 +118,11 @@ public class Solution {
         return true;
     }
 
-    private static boolean checkDomen(String address) {
+    private boolean checkDomen(String address) {
         return address.substring(address.length() - 4).equals(".com") || address.substring(address.length() - 4).equals(".net") || address.substring(address.length() - 4).equals(".com");
     }
 
-    private static boolean checkHttp(String address) {
+    private boolean checkHttp(String address) {
         return address.substring(0, 7).equals("http://") && checkAddress(address.substring(7, address.length() - 4))
                 || address.substring(0, 8).equals("https://") && checkAddress(address.substring(8, address.length() - 4))
                 || address.substring(0, 11).equals("http://www.") && checkAddress(address.substring(11, address.length() - 4))
@@ -34,4 +130,3 @@ public class Solution {
     }
 
 }
-
