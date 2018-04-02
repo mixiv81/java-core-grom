2409673fc64e56f81f0e7f66cc8f168c968bb091
Ihor <37148638+mixiv81@users.noreply.github.com>
2018-04-02 15:51:39 +0300

    Add files via upload

--- src/lesson17/Solution.java
+++ src/lesson17/Solution.java
@@ -4,30 +4,22 @@ public class Solution {
 
     public boolean validate(String address) {
         return address != null && !address.isEmpty()
-                && checkHttpAndSymbol(address)
-                && checkDomens(address);
+                && checkNameAddress(address)
+                && checkDomensAndProtocols(address);
     }
 
-    private boolean checkHttpAndSymbol(String address) {
-        return (address.startsWith("http://")
-                || address.startsWith("https://")
-                || address.startsWith("http://www.")
-                || address.startsWith("https://www.")
-                && checkAddress(getNameAddress(address)));
+    private boolean checkDomensAndProtocols(String address) {
+        return (address.startsWith("http://") || address.startsWith("https://") && address.endsWith(".com") || address.endsWith(".net") || address.endsWith(".org"));
     }
 
-    public String getNameAddress(String address) {
-        address = address.replace("http://", "");
-        address = address.replace("https://", "");
-        address = address.replace("www.", "");
-        address = address.replace(".com", "");
-        address = address.replace(".net", "");
-        address = address.replace(".org", "");
-        return address;
-    }
-
-    private boolean checkDomens(String address) {
-        return address.endsWith(".com") || address.endsWith(".net") || address.endsWith(".org");
+    private boolean checkNameAddress(String address) {
+        address = address.replaceAll("http://", "");
+        address = address.replaceAll("https://", "");
+        address = address.replaceAll("www.", "");
+        address = address.replaceAll(".com", "");
+        address = address.replaceAll(".net", "");
+        address = address.replaceAll(".org", "");
+        return checkAddress(address);
     }
 
     private boolean checkAddress(String input) {
