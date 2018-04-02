47b646f7e4d63c819cf34854eed6df9548da8b81
Ihor <37148638+mixiv81@users.noreply.github.com>
2018-04-02 15:20:14 +0300

    Add files via upload

--- src/lesson17/Solution.java
+++ src/lesson17/Solution.java
@@ -4,20 +4,30 @@ public class Solution {
 
     public boolean validate(String address) {
         return address != null && !address.isEmpty()
-                && checkDomes(address)
-                && checkHttpAndSymbol(address);
+                && checkHttpAndSymbol(address)
+                && checkDomens(address);
     }
 
-    private boolean checkDomes(String address) {
-        return (address.substring(address.length() - 4).equals(".com")
-                || address.substring(address.length() - 4).equals(".org")
-                || address.substring(address.length() - 4).equals(".net"));
-    }
     private boolean checkHttpAndSymbol(String address) {
-        return (address.substring(0, 7).equals("http://") && checkAddress(address.substring(7, address.length() - 4))
-                || address.substring(0, 8).equals("https://") && checkAddress(address.substring(8, address.length() - 4))
-                || address.substring(0, 11).equals("http://www.") && checkAddress(address.substring(11, address.length() - 4))
-                || address.substring(0, 12).equals("https://www.") && checkAddress(address.substring(12, address.length() - 4)));
+        return (address.startsWith("http://")
+                || address.startsWith("https://")
+                || address.startsWith("http://www.")
+                || address.startsWith("https://www.")
+                && checkAddress(getNameAddress(address)));
+    }
+
+    public String getNameAddress(String address) {
+        address = address.replace("http://", "");
+        address = address.replace("https://", "");
+        address = address.replace("www.", "");
+        address = address.replace(".com", "");
+        address = address.replace(".net", "");
+        address = address.replace(".org", "");
+        return address;
+    }
+
+    private boolean checkDomens(String address) {
+        return address.endsWith(".com") || address.endsWith(".net") || address.endsWith(".org");
     }
 
     private boolean checkAddress(String input) {
@@ -27,7 +37,4 @@ public class Solution {
         }
         return true;
     }
-
-
-
 }
