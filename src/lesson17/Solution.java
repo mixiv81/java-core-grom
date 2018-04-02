f400ebf0c11bd1eeee22e6e9e48be221b67021da
Ihor <37148638+mixiv81@users.noreply.github.com>
2018-04-02 14:17:07 +0300

    Add files via upload

--- src/lesson17/Solution.java
+++ src/lesson17/Solution.java
@@ -8,6 +8,18 @@ public class Solution {
                 && checkHttpAndSymbol(address);
     }
 
+    private boolean checkDomes(String address) {
+        return (address.substring(address.length() - 4).equals(".com")
+                || address.substring(address.length() - 4).equals(".org")
+                || address.substring(address.length() - 4).equals(".net"));
+    }
+    private boolean checkHttpAndSymbol(String address) {
+        return (address.substring(0, 7).equals("http://") && checkAddress(address.substring(7, address.length() - 4))
+                || address.substring(0, 8).equals("https://") && checkAddress(address.substring(8, address.length() - 4))
+                || address.substring(0, 11).equals("http://www.") && checkAddress(address.substring(11, address.length() - 4))
+                || address.substring(0, 12).equals("https://www.") && checkAddress(address.substring(12, address.length() - 4)));
+    }
+
     private boolean checkAddress(String input) {
         for (char ch : input.toCharArray()) {
             if (!Character.isLetterOrDigit(ch))
@@ -16,16 +28,6 @@ public class Solution {
         return true;
     }
 
-    private boolean checkDomes(String address) {
-        return (address.substring(address.length() - 4).equals(".com")
-                || address.substring(address.length() - 4).equals(".org")
-                || address.substring(address.length() - 4).equals(".net"));
-    }
 
-    private boolean checkHttpAndSymbol(String address) {
-        return (address.substring(0, 7).equals("http://") && checkAddress(address.substring(7, address.length() - 4))
-                || address.substring(0, 8).equals("https://") && checkAddress(address.substring(8, address.length() - 4))
-                || address.substring(0, 11).equals("http://www.") && checkAddress(address.substring(11, address.length() - 4))
-                || address.substring(0, 12).equals("https://www.") && checkAddress(address.substring(12, address.length() - 4)));
-    }
+
 }
