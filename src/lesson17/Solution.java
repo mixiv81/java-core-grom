794a9cc08901397bfb16ed046f20feb9a327607f
Ihor <37148638+mixiv81@users.noreply.github.com>
2018-04-02 13:12:10 +0300

    Add files via upload

--- src/lesson17/Solution.java
+++ src/lesson17/Solution.java
@@ -79,7 +79,7 @@ public class Solution {
     public boolean validate(String address) {
         return address != null && !address.isEmpty()
                 && checkHttp(address)
-                && checkDomen(address);
+                && checkDomes(address);
     }
 
 
@@ -109,25 +109,21 @@ public class Solution {
     }
 
     private boolean checkAddress(String input) {
-        if (input == null)
-            return false;
-        if (input.isEmpty())
-            return false;
-        for (char chars : input.toCharArray()) {
-            if (!Character.isLetterOrDigit(chars))
+        for (char ch : input.toCharArray()) {
+            if (!Character.isLetterOrDigit(ch))
                 return false;
         }
         return true;
     }
 
-    private boolean checkDomen(String address) {
-        return address != null && !address.isEmpty() && (address.substring(address.length() - 4).equals(".com")
+    private boolean checkDomes(String address) {
+        return (address.substring(address.length() - 4).equals(".com")
                 || address.substring(address.length() - 4).equals(".net")
                 || address.substring(address.length() - 4).equals(".com"));
     }
 
     private boolean checkHttp(String address) {
-        return address != null && !address.isEmpty() && (address.substring(0, 7).equals("http://") && checkAddress(address.substring(7, address.length() - 4))
+        return (address.substring(0, 7).equals("http://") && checkAddress(address.substring(7, address.length() - 4))
                 || address.substring(0, 8).equals("https://") && checkAddress(address.substring(8, address.length() - 4))
                 || address.substring(0, 11).equals("http://www.") && checkAddress(address.substring(11, address.length() - 4))
                 || address.substring(0, 12).equals("https://www.") && checkAddress(address.substring(12, address.length() - 4)));
