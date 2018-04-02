8641f8ca494f933ca0b741d7c88b624d6ef115f5
Ihor <37148638+mixiv81@users.noreply.github.com>
2018-04-02 12:55:45 +0300

    Add files via upload

--- src/lesson17/Solution.java
+++ src/lesson17/Solution.java
@@ -77,7 +77,9 @@ public class Solution {
     }
 
     public boolean validate(String address) {
-        return address != null && !address.isEmpty() && checkHttp(address);
+        return address != null && !address.isEmpty()
+                && checkHttp(address)
+                && checkDomen(address);
     }
 
 
@@ -119,14 +121,16 @@ public class Solution {
     }
 
     private boolean checkDomen(String address) {
-        return address.substring(address.length() - 4).equals(".com") || address.substring(address.length() - 4).equals(".net") || address.substring(address.length() - 4).equals(".com");
+        return address != null && !address.isEmpty() && (address.substring(address.length() - 4).equals(".com")
+                || address.substring(address.length() - 4).equals(".net")
+                || address.substring(address.length() - 4).equals(".com"));
     }
 
     private boolean checkHttp(String address) {
-        return address.substring(0, 7).equals("http://") && checkAddress(address.substring(7, address.length() - 4))
+        return address != null && !address.isEmpty() && (address.substring(0, 7).equals("http://") && checkAddress(address.substring(7, address.length() - 4))
                 || address.substring(0, 8).equals("https://") && checkAddress(address.substring(8, address.length() - 4))
                 || address.substring(0, 11).equals("http://www.") && checkAddress(address.substring(11, address.length() - 4))
-                || address.substring(0, 12).equals("https://www.") && checkAddress(address.substring(12, address.length() - 4));
+                || address.substring(0, 12).equals("https://www.") && checkAddress(address.substring(12, address.length() - 4)));
     }
 
 }
