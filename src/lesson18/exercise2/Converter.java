d431aecc22156a3891f6cccdcb6e5d7d75936a99
Ihor <37148638+mixiv81@users.noreply.github.com>
2018-04-06 13:45:59 +0300

    Add files via upload

--- src/lesson18/exercise2/Converter.java
+++ src/lesson18/exercise2/Converter.java
@@ -6,6 +6,14 @@ public class Converter {
         System.out.println(stringToInt("11"));
 
         System.out.println(stringToInt("1050a"));
+        System.out.println(stringToInt(""));
+        System.out.println(stringToInt("."));
+        System.out.println(stringToInt(null));
+        System.out.println(stringToInt("   "));
+
+        System.out.println(intToString(0));
+        System.out.println(intToString(123));
+
 
         System.out.println(intToString(111231));
         System.out.println(stringToInt("11"));
@@ -15,9 +23,13 @@ public class Converter {
     }
 
     public static String intToString(int input) {
-        return String.valueOf(input);
+        try {
+            return String.valueOf(input);
+        }catch (Exception e){
+            System.out.println(input + " can not be converted to String");
+            return null;
+        }
     }
-
     public static int stringToInt(String input) {
         try {
             return Integer.parseInt(input);
