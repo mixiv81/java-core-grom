ca6e3e2133834fff6a3918bac58e28763c1fa013
Ihor <37148638+mixiv81@users.noreply.github.com>
2018-04-06 14:58:57 +0300

    Add files via upload

---
+++ src/lesson18/SolutionV1/Solution.java
@@ -0,0 +1,39 @@
+package lesson18.numbersV1;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String test = "   Ther e is Test  55 sdf / 0  b 5 or new 100 o5r sdfs  b jot. something  word op o5r";
+        String test1 = null;
+        String test2 = "";
+        String test3 = "4.5 5 8,0 kkl  ";
+        String test4 = "0";
+
+
+        // System.out.println(Arrays.toString(findNumbers(test)));
+        System.out.println(Arrays.toString(findNumbers(test1)));
+        System.out.println(Arrays.toString(findNumbers(test2)));
+        System.out.println(Arrays.toString(findNumbers(test3)));
+        System.out.println(Arrays.toString(findNumbers(test4)));
+
+    }
+
+    public static int[] findNumbers(String text) {
+        if (text == null || text.isEmpty())
+            return null;
+        String[] words = text.trim().split(" ");
+        int[] numbers = new int[words.length];
+        int count = 0;
+        for (String word : words) {
+            try {
+                numbers[count] = Integer.parseInt(word);
+                count++;
+            } catch (Exception e) {
+                System.out.println(word + " not a number");
+            }
+        }
+        return numbers;
+    }
+}
