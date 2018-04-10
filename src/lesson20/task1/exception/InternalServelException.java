d34270e867f54f05a5007bf5c2f29e831766f8af
mixiv81 <zhluktenkoiv@gmail.com>
2018-04-10 19:54:54 +0300

    add lesson20 UserRepository

---
+++ src/lesson20/task1/exception/InternalServelException.java
@@ -0,0 +1,8 @@
+package lesson20.task1.exception;
+
+public class InternalServelException extends Exception{
+
+    public InternalServelException(String message) {
+        super(message);
+    }
+}
