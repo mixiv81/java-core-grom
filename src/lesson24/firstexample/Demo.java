141cecb60dcd5d2591d1dd661c890bc8aa751ecb
mixiv81 <zhluktenkoiv@gmail.com>
2018-04-23 22:02:49 +0300

    add lesson24

---
+++ src/lesson24/firstexample/Demo.java
@@ -0,0 +1,12 @@
+package lesson24.firstexample;
+
+public class Demo<T> {
+
+    public void print(T t){
+        System.out.println(t);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
