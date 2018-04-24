7cbbf31f299905e2adc28835c935ca0d8e97f681
Ihor <37148638+mixiv81@users.noreply.github.com>
2018-04-24 10:14:56 +0300

    Add files via upload

---
+++ src/lesson25/Demo.java
@@ -0,0 +1,22 @@
+package lesson25;
+
+public class Demo {
+    public static void main(String[] args) throws Exception {
+        GeneralDAO generalDAO = new GeneralDAO();
+
+        Order order = new Order(1);
+        generalDAO.validate(order);
+
+        TestClass<String, Order, Long> testClass = new TestClass<>();
+        System.out.println(testClass.doSomething1("rrr"));
+
+        int test = 100;
+        long variable = 111;
+
+        Long variable2 = 222L;
+
+        generalDAO.validate(variable2);
+        generalDAO.validate(variable);
+        generalDAO.validate(test);
+    }
+}
