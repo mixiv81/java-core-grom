828bb91474b95990653e2f50b8d15a0b4d07f2b0
mixiv81 <zhluktenkoiv@gmail.com>
2018-05-10 14:00:32 +0300

    add lesson29. HW

---
+++ src/lesson29/Demo.java
@@ -0,0 +1,26 @@
+package lesson29;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        Order order = new Order(1, 10, "usd", "aa", "aa");
+        Order order1 = new Order(2, 20, "usd", "bb", "bb");
+        Order order2 = new Order(3, 30, "usd", "cc", "cc");
+        Order order4 = new Order(4, 40, "usd", "dd", "dd");
+        Order order5 = new Order(5, 50, "usd", "ee", "ee");
+
+        HashSetTest hashSetTest = new HashSetTest();
+
+        Set<Order> orderSet = new HashSet<>();
+        orderSet.add(order);
+        orderSet.add(order1);
+        orderSet.add(order2);
+        orderSet.add(order4);
+        orderSet.add(order5);
+
+        hashSetTest.useHashSet(orderSet);
+    }
+}
---
+++ src/lesson29/Order.java
@@ -0,0 +1,64 @@
+package lesson29;
+
+import java.util.Objects;
+
+public class Order {
+    private long id;
+    private int price;
+    private String currency;
+    private String itemName;
+    private String shopIdentificator;
+
+    public Order(long id, int price, String currency, String itemName, String shopIdentificator) {
+        this.id = id;
+        this.price = price;
+        this.currency = currency;
+        this.itemName = itemName;
+        this.shopIdentificator = shopIdentificator;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public String getShopIdentificator() {
+        return shopIdentificator;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return id == order.id;
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", price=" + price +
+                ", currency='" + currency + '\'' +
+                ", itemName='" + itemName + '\'' +
+                ", shopIdentificator='" + shopIdentificator + '\'' +
+                '}';
+    }
+}
