03fcb574c6b8230bbf0de832f507c2a63e240e12
mixiv81 <zhluktenkoiv@gmail.com>
2018-06-12 23:15:55 +0300

    add lesson32

---
+++ src/lesson32/Demo.java
@@ -0,0 +1,9 @@
+package lesson32;
+
+public class Demo {
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+        System.out.println(solution.readNumbers());
+    }
+}
