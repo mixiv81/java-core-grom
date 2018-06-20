a21e873f54babdf7ad6b631a7b8b13a07c306c6c
mixiv81 <zhluktenkoiv@gmail.com>
2018-06-20 06:09:21 +0300

    add lesson34 task

---
+++ src/lesson34/Demo.java
@@ -0,0 +1,8 @@
+package lesson34;
+
+public class Demo {
+    public static void main(String[] args) throws Exception {
+        Solution solution = new Solution();
+        solution.copyFileContent("C:/Users/User/Desktop/fileFrom.txt", "C:/Users/User/Desktop/fileTo.txt");
+    }
+}
---
+++ src/lesson34/Solution.java
@@ -0,0 +1,58 @@
+package lesson34;
+
+import java.io.*;
+
+public class Solution {
+    public void copyFileContent(String fileFromPath, String fileToPath) throws Exception {
+
+        validate(fileFromPath, fileToPath);
+
+        writeToFile(fileToPath, readFromFile(fileFromPath));
+
+    }
+
+    private StringBuffer readFromFile(String path) throws IOException {
+        StringBuffer res = new StringBuffer();
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                res.append(line);
+                res.append("\n");
+            }
+            res.replace(res.length() - 1, res.length(), "");
+        } catch (IOException e) {
+            throw new IOException("Reading from file " + path + " failed");
+        }
+        return res;
+    }
+
+    private void writeToFile(String path, StringBuffer contentToWrite) throws IOException {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(path, true))) {
+            bufferedWriter.append(contentToWrite);
+        } catch (IOException e) {
+            throw new IOException("Writing to file " + path + " failed");
+        }
+    }
+
+
+    private void validate(String fileFromPath, String fileToPath) throws Exception {
+        File fileFrom = new File(fileFromPath);
+        File fileTo = new File(fileToPath);
+
+        if (!fileFrom.exists()) {
+            throw new FileNotFoundException("File " + fileFromPath + " does not exist");
+        }
+
+        if (!fileTo.exists()) {
+            throw new FileNotFoundException("File " + fileTo + " does not exist");
+        }
+
+        if (!fileFrom.canRead()) {
+            throw new Exception("File " + fileFromPath + " does not have permission to be read");
+        }
+
+        if (!fileTo.canWrite()) {
+            throw new Exception("File " + fileTo + " does not have permission to be read");
+        }
+    }
+}
