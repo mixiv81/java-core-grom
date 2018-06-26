7b76e41e882ac537b738b6aa581382415e14072f
Ihor <37148638+mixiv81@users.noreply.github.com>
2018-06-26 09:17:09 +0300

    Create Demo.java

---
+++ src/lesson34/HW/Demo.java
@@ -0,0 +1,8 @@
+package lesson34.HW;
+
+public class Demo {
+    public static void main(String[] args) throws Exception {
+        Solution solution = new Solution();
+        solution.transferFileContent("C:/Users/Игорь/Desktop/fileFrom.txt", "C:/Users/Игорь/Desktop/fileTo.txt");
+    }
+}
