75ae3a36c07d14522ad49a0360c92093ec75447e
Ihor <37148638+mixiv81@users.noreply.github.com>
2018-06-27 12:58:53 +0300

    add HW2 Demo

---
+++ src/lesson34/HW2/Demo.java
@@ -0,0 +1,8 @@
+package lesson34.HW2;
+
+public class Demo {
+    public static void main(String[] args) throws Exception {
+        Solution solution = new Solution();
+        solution.transferSentences("C:/Users/Игорь/Desktop/fileFrom.txt", "C:/Users/Игорь/Desktop/fileTo.txt", "ggg");
+    }
+}
